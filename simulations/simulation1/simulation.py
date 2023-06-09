cb5486bc00850f4577f7a9e64eca53a3fe0ace81
CaipirUltron <matheus.ferreira.reis@gmail.com>
2023-06-09 17:21:16 +0100

    Add simulation1 example (barriers and paths)

--- simulations/simulation1/simulation.py
+++ simulations/simulation1/simulation.py
@@ -7,23 +7,24 @@ from simulations.load_splines import *
 
 ########################################### Configure and create robots ####################################################
 pos_offset = 1.0
-robot1 = Unicycle( initial_state=[ -2.0, 2.0, 0.0 ], initial_control=np.zeros(2), geometric_params=Rect([3.0, 1.5], 1.0) )
+robot1 = Unicycle( initial_state=[ -5.0, 10.0, 0.0 ], initial_control=np.zeros(2), geometric_params=Rect([3.0, 1.5], 1.0) )
+
 robots = [ robot1 ]
 
 ########################################### Load splines and create paths ##################################################
-
+paths[0].set_path_state(0.0)
 
 ############################################# Configure and create controllers #############################################
-path_speed = 5.5
+path_speed = 6.0
 radius = 1.0
 sample_time = 0.005
 
-controller_parameters = { 
+controller_parameters1 = { 
     "sample_time": sample_time, "path": paths[0], "path_speed": path_speed,
     "q1": 1.0, "q2": 10.0, "alpha1": 100.0, "alpha2": 10.0, "beta0": 1.0, "radius": radius }
 
 # Create QP controller and graphical simulation.
-controller1 = PFController(robots, barriers, controller_parameters, id=0)
+controller1 = PFController(robots, barriers, controller_parameters1, id=0)
 
 controllers = [ controller1 ]
 
